2c71cd6b6f8d20e4db4a2a56b09559b888e226e3
Rathish-1109001 <54340082+Rathish-1109001@users.noreply.github.com>
2021-06-26 18:56:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javasript and Reat.js</h1>
+      <p>A basic web dev React.js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
